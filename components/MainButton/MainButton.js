66319e026914c2e159c6ad5b29dcaa77f6e55837
Alan2304 <sanchezalan95@gmail.com>
2019-09-15 11:03:14 -0500

    Add custom button component

---
+++ components/MainButton/MainButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+const MainButton = props => {
+    return (
+        <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>{props.children}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+export default MainButton;
---
+++ components/MainButton/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+import Colors from '../../constants/colors';
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: Colors.primary,
+        paddingVertical: 12,
+        paddingHorizontal: 30,
+        borderRadius: 25
+    },
+    buttonText: {
+        color: 'white',
+        fontFamily: 'open-sans',
+        fontSize: 18
+    }
+});
+
+export default styles;  
